05d7189c80d7f1dbca912ef4ac90fbbfcbe52093
Destrudo23 <51265520+Destrudo23@users.noreply.github.com>
2023-04-03 21:26:33 +0200

    1 file modified, 1 added

---
+++ data.js
@@ -0,0 +1,37 @@
+export const emojiData = [
+  {
+    colorTags: ["yellow"],
+    isFruit: true,
+    image: "🍋",
+  },
+  {
+    colorTags: ["red", "green"],
+    isFruit: true,
+    image: "🍉",
+  },
+  {
+    colorTags: ["red", "green"],
+    isFruit: false,
+    image: "🍅",
+  },
+  {
+    colorTags: ["green", "yellow"],
+    isFruit: false,
+    image: "🌽",
+  },
+  {
+    colorTags: ["green"],
+    isFruit: false,
+    image: "🥦",
+  },
+  {
+    colorTags: ["green", "brown"],
+    isFruit: true,
+    image: "🥝",
+  },
+  {
+    colorTags: ["brown"],
+    isFruit: false,
+    image: "🧅",
+  },
+]
--- script.js
+++ script.js
@@ -1,40 +1,5 @@
-const emojiData = [
-    {
-      colorTags: ["yellow"],
-      isFruit: true,
-      image: "🍋",
-    },
-    {
-      colorTags: ["red", "green"],
-      isFruit: true,
-      image: "🍉",
-    },
-    {
-      colorTags: ["red", "green"],
-      isFruit: false,
-      image: "🍅",
-    },
-    {
-      colorTags: ["green", "yellow"],
-      isFruit: false,
-      image: "🌽",
-    },
-    {
-      colorTags: ["green"],
-      isFruit: false,
-      image: "🥦",
-    },
-    {
-      colorTags: ["green", "brown"],
-      isFruit: true,
-      image: "🥝",
-    },
-    {
-      colorTags: ["brown"],
-      isFruit: false,
-      image: "🧅",
-    },
-]
+import {emojiData} from "./data.js"
+
 
 const radioButtonsList = document.getElementById("color-radios");
 
